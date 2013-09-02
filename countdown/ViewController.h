6b45ed0960f01487524be6ca1108eb42b7e5462c
Veeral Patel <v@veeralpatel.com>
2013-09-02 01:13:43 -0400

    Initial Commit

---
+++ countdown/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  countdown
+//
+//  Created by Veeral Patel on 9/2/13.
+//  Copyright (c) 2013 Veeral Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ countdown/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  countdown
+//
+//  Created by Veeral Patel on 9/2/13.
+//  Copyright (c) 2013 Veeral Patel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
